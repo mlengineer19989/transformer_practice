1a3704d5213ea158f09ad381d7cbd91ff8947335
satoshi19989 <3104monogotueeko@gmail.com>
2023-02-23 07:57:24 +0900

    (add) generate_data.py編集

--- modules/generate_data.py
+++ modules/generate_data.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+from keras.preprocessing.sequence import TimeseriesGenerator
 from utility import resampling
 
 
@@ -151,4 +152,49 @@ def textbook_method(raw_data, temperature, sampling_rate, sequence_length, delay
       shuffle=True,
       batch_size=batch_size,
       start_index=num_train_samples + num_val_samples)
+  return train_dataset, val_dataset, test_dataset
+
+
+def textbook_method_multistep(raw_data, temperature, sampling_rate, sequence_length, delay, pred_points, batch_size):
+  num_train_samples = int(0.5 * len(raw_data))
+  num_val_samples = int(0.25 * len(raw_data))
+  num_test_samples = len(raw_data) - num_train_samples - num_val_samples
+  print("num_train_samples:", num_train_samples)
+  print("num_val_samples:", num_val_samples)
+  print("num_test_samples:", num_test_samples)
+
+  mean = raw_data[:num_train_samples].mean(axis=0)
+  raw_data -= mean
+  std = raw_data[:num_train_samples].std(axis=0)
+  raw_data /= std
+
+  tar_temp = np.array([temperature[i+delay : i+delay+pred_points] for i in range(len(temperature[delay:-pred_points]))])
+  train_dataset = TimeseriesGenerator(raw_data[:-delay-pred_points], 
+                                      tar_temp, 
+                                      length=sequence_length, 
+                                      sampling_rate=sampling_rate, 
+                                      shuffle=True,
+                                      batch_size=batch_size,
+                                      start_index=0,
+                                      end_index=num_train_samples)
+
+  
+  val_dataset = TimeseriesGenerator(raw_data[:-delay-pred_points], 
+                                      tar_temp, 
+                                      length=sequence_length, 
+                                      sampling_rate=sampling_rate, 
+                                      shuffle=True,
+                                      batch_size=batch_size,
+                                      start_index=num_train_samples,
+                                      end_index=num_train_samples + num_val_samples)
+
+  
+  test_dataset = TimeseriesGenerator(raw_data[:-delay-pred_points], 
+                                      tar_temp, 
+                                      length=sequence_length, 
+                                      sampling_rate=sampling_rate, 
+                                      shuffle=True,
+                                      batch_size=batch_size,
+                                      start_index=num_train_samples + num_val_samples)
+  
   return train_dataset, val_dataset, test_dataset
