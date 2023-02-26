6db2d939bf3264554ae8a5652dfd7165a437de20
satoshi19989 <3104monogotueeko@gmail.com>
2023-02-26 09:08:57 +0900

    (add) generate_data.py

--- modules/generate_data.py
+++ modules/generate_data.py
@@ -6,27 +6,43 @@ from utility import resampling
 
 
 class PreprocessData():
-  def __init__(self, raw_data, batch_size, window_width, pred_points,  resampling_rate=None, output_index=None):
+  """学習用データセット生成クラス。以下を実行する。
+    ・train, validation, testデータへの分割
+    ・説明変数データの正規化
+    ・1ステップ予測用データセット生成
+    ・seq2seq用データセット生成
+  """
+  def __init__(self, 
+              raw_data, 
+              temperature, 
+              sequence_length : int, 
+              delay : int, 
+              batch_size: int, 
+              sampling_rate : int=None):
+    """
+    Args:
+        raw_data (_type_): 説明変数の配列
+        temperature (_type_): 目的変数の配列
+        sequence_length (int): 説明変数時系列の時系列長
+        delay (int): 説明変数の先頭インデックスに対する、目的変数の先頭インデックスの時間遅れ
+        batch_size (int): ミニバッチの説明変数と目的変数の組み合わせの個数。
+        sampling_rate (int, optional): データリサンプリングの間隔. Defaults to None.
+    """
     self.raw_data = raw_data
+    self.targets = temperature
+    self.sequence_length = sequence_length
+    self.delay = delay
     self.batch_size = batch_size
-    self.window_width = window_width
-    self.pred_points = pred_points
-    self.output_index = output_index
+    self.sampling_rate = sampling_rate
 
-    if resampling_rate:
-      self.raw_data = resampling(self.raw_data, resampling_rate)
-
-    self.num_train_samples = int(0.5 * len(self.raw_data)//self.batch_size * self.batch_size) + self.window_width +self.pred_points
-    self.num_val_samples = int(0.25 * len(self.raw_data)//self.batch_size * self.batch_size) + self.window_width +self.pred_points
-    self.num_test_samples = (len(self.raw_data) - self.num_train_samples - self.num_val_samples)//self.batch_size * self.batch_size - self.batch_size + self.window_width +self.pred_points
-
-    self.normalization()
-
+    self.num_train_samples = int(0.5 * len(raw_data))
+    self.num_val_samples = int(0.25 * len(raw_data))
+    self.num_test_samples = len(raw_data) - self.num_train_samples - self.num_val_samples
+    print("num_train_samples:", self.num_train_samples)
+    print("num_val_samples:", self.num_val_samples)
+    print("num_test_samples:", self.num_test_samples)
     
-
-    self.train_data = self.raw_data[: self.num_train_samples]
-    self.val_data = self.raw_data[self.num_train_samples : self.num_train_samples + self.num_val_samples]
-    self.test_data = self.raw_data[self.num_train_samples + self.num_val_samples : self.num_train_samples + self.num_val_samples + self.num_test_samples]
+    self.mean, self.std = self.normalization()
 
   def normalization(self):
     #正規化
@@ -34,81 +50,111 @@ class PreprocessData():
     self.raw_data -= mean
     std = self.raw_data[:self.num_train_samples].std(axis=0)
     self.raw_data /= std
+    return mean, std
 
-  def splitdata_for_transformer(self, emb_data):
-    enc = np.array([emb_data[i:i+self.window_width] for i in range(len(emb_data)-self.window_width-self.pred_points)])
-    dec = np.array([emb_data[i+self.window_width-1:i+self.window_width-1+self.pred_points] for i in range(len(emb_data)-self.window_width-self.pred_points)])
-    
-    if self.output_index:
-      y = np.array([emb_data[i+self.window_width:i+self.window_width+self.pred_points, self.output_index] for i in range(len(emb_data)-self.window_width-self.pred_points)])
-    else:
-      y = np.array([emb_data[i+self.window_width:i+self.window_width+self.pred_points] for i in range(len(emb_data)-self.window_width-self.pred_points)])
+  def generate_dataset_single(self):
+    train_dataset = keras.utils.timeseries_dataset_from_array(
+        self.raw_data[:-self.delay],
+        targets=self.targets[self.delay:],
+        sampling_rate=self.sampling_rate,
+        sequence_length=self.sequence_length,
+        shuffle=True,
+        batch_size=self.batch_size,
+        start_index=0,
+        end_index=self.num_train_samples)
 
-    return enc, dec, y
+    val_dataset = keras.utils.timeseries_dataset_from_array(
+        self.raw_data[:-self.delay],
+        targets=self.targets[self.delay:],
+        sampling_rate=self.sampling_rate,
+        sequence_length=self.sequence_length,
+        shuffle=True,
+        batch_size=self.batch_size,
+        start_index=self.num_train_samples,
+        end_index=self.num_train_samples + self.num_val_samples)
 
+    test_dataset = keras.utils.timeseries_dataset_from_array(
+        self.raw_data[:-self.delay],
+        targets=self.targets[self.delay:],
+        sampling_rate=self.sampling_rate,
+        sequence_length=self.sequence_length,
+        shuffle=True,
+        batch_size=self.batch_size,
+        start_index=self.num_train_samples + self.num_val_samples)
+    return train_dataset, val_dataset, test_dataset
 
-  def splitdata_for_NN(self, emb_data):
-      inp = np.array([emb_data[i:i+self.window_width] for i in range(len(emb_data)-self.window_width-self.pred_points)])
-      
-      if self.output_index:
-        y = np.array([emb_data[i+self.window_width+self.pred_points, self.output_index] for i in range(len(emb_data)-self.window_width-self.pred_points)])
+  def generate_dataset_multistep(self, pred_points : int):
+    """
+    seq2seq用データセットを生成する。tensorflowには、以下のように時系列と時系列が組となるseq2seq用データセット生成メソッドがない。
+    [enc_seq, dec_seq] => target_seq
+    したがって、時系列と単一ステップのデータの組を生成するTimeseriesGeneratorを活用する。
+
+    例えば、
+    raw_data = [0, 1, 2, 3, 4, 5, 6, ...]
+    から、
+    len(enc_seq), len(dec_seq), len(target_seq) = 3, 2, 2
+    となるような以下のデータセットを生成したい場合を考える。
+    [0, 1, 2, 3] -> [3, 4]
+    [1, 2, 3, 4] -> [4, 5]
+    [2, 3, 4, 5] -> [5, 6]
+              .
+              .
+              .
+
+    TimeseriesGeneratorの目的変数には以下のようなデータを与えることで実現できる。
+    tar_temp = [None, None, None, None, [3, 4], [4, 5], [5, 6]]
+    TimeseriesGeneratorでは、入力系列[enc_seq, dec_seq]の長さ分進んだインデックスから目的変数を取得する。
+    したがって、上記配列はその分ずらすために入力系列長分だけNoneを先頭に与えている。
+    ただし、本対象はエンコーダ入力系列末尾のインデックスとデコーダ入力系列先頭のインデックスを一致させているため、
+    入力系列長は、len(enc_seq) + len(dec_seq) -1となることに注意。
+
+    Args:
+        pred_points (int): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    input_length = self.sequence_length + pred_points -1
+    sequence_length = self.sampling_rate * self.sequence_length
+
+    #tar_temp生成
+    tar_temp = []
+    for i in range(len(self.targets[:-self.delay-pred_points])):
+      if i < pred_points:
+        tar_temp.append(None)
       else:
-        y = np.array([emb_data[i+self.window_width+self.pred_points] for i in range(len(emb_data)-self.window_width-self.pred_points)])
-      return inp, y
+        tar_temp.append(self.targets[i+1-pred_points : i+1])
+    tar_temp = np.array(tar_temp)
 
-  def make_data_for_transformer(self):
-    enc_train, dec_train, y_train = self.splitdata_for_transformer(self.train_data)
-    enc_val, dec_val, y_val = self.splitdata_for_transformer(self.val_data)
-    enc_test, dec_test, y_test = self.splitdata_for_transformer(self.test_data)
+    train_dataset = TimeseriesGenerator(self.raw_data[:-self.delay-pred_points], 
+                                        tar_temp, 
+                                        length=input_length, 
+                                        sampling_rate=self.sampling_rate, 
+                                        shuffle=True,
+                                        batch_size=self.batch_size,
+                                        start_index=0,
+                                        end_index=self.num_train_samples)
 
+    
+    val_dataset = TimeseriesGenerator(self.raw_data[:-self.delay-pred_points], 
+                                        tar_temp, 
+                                        length=input_length, 
+                                        sampling_rate=self.sampling_rate, 
+                                        shuffle=True,
+                                        batch_size=self.batch_size,
+                                        start_index=self.num_train_samples,
+                                        end_index=self.num_train_samples + self.num_val_samples)
 
-    enc_train = tf.constant(enc_train)
-    enc_val = tf.constant(enc_val)
-    enc_test = tf.constant(enc_test)
-    dec_train = tf.constant(dec_train)
-    dec_val = tf.constant(dec_val)
-    dec_test = tf.constant(dec_test)
-    y_train = tf.constant(y_train)
-    y_val = tf.constant(y_val)
-    y_test = tf.constant(y_test)
-
-    print('encinput_train ', enc_train.shape)
-    print('decinput_train ', dec_train.shape)
-    print('y_train ', y_train.shape)
-
-    print('encinput_val ', enc_val.shape)
-    print('decinput_val ', dec_val.shape)
-    print('y_val ', y_val.shape)
-
-    print('encinput_test ', enc_test.shape)
-    print('decinput_test ', dec_test.shape)
-    print('y_test ', y_test.shape)
-
-    return enc_train, dec_train, y_train, enc_val, dec_val, y_val, enc_test, dec_test, y_test
-
-  def make_data_for_NN(self):
-    x_train, y_train = self.splitdata_for_NN(self.train_data)
-    x_val, y_val = self.splitdata_for_NN(self.val_data)
-    x_test, y_test = self.splitdata_for_NN(self.test_data)
-
-
-    x_train = tf.constant(x_train)
-    x_val = tf.constant(x_val)
-    x_test = tf.constant(x_test)
-    y_train = tf.constant(y_train)
-    y_val = tf.constant(y_val)
-    y_test = tf.constant(y_test)
-
-    print('input_train ', x_train.shape)
-    print('y_train ', y_train.shape)
-
-    print('input_val ', x_val.shape)
-    print('y_val ', y_val.shape)
-
-    print('input_test ', x_test.shape)
-    print('y_test ', y_test.shape)
-    return x_train, y_train, x_val, y_val, x_test, y_test
-
+    
+    test_dataset = TimeseriesGenerator(self.raw_data[:-self.delay-pred_points], 
+                                        tar_temp, 
+                                        length=input_length, 
+                                        sampling_rate=self.sampling_rate, 
+                                        shuffle=False,
+                                        batch_size=self.batch_size,
+                                        start_index=self.num_train_samples + self.num_val_samples)
+    
+    return train_dataset, val_dataset, test_dataset
 
 
 def textbook_method(raw_data, temperature, sampling_rate, sequence_length, delay, batch_size):
